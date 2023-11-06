f7a8c81d68bd7090fd239c2c22db505d75e0abe2
Michael Tomadakis <c1a55yc0d3r@gmail.com>
2023-11-06 12:59:48 -0500

    migrate public, package.json

--- client-ts/client-retooled/README.md
+++ client-ts/client-retooled/README.md
@@ -1,27 +1,71 @@
-# React + TypeScript + Vite
+# Client
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+> [React.js](https://reactjs.org/) application which represents the entire browser-side client. File structure is standard for a [create-react-app](https://github.com/facebook/create-react-app) application with some minor changes. Any component which represents an entire page is categorized under the [views](/client/src/views) folder.
 
-Currently, two official plugins are available:
+<br />
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Setup
 
-## Expanding the ESLint configuration
+1. Install [Node](https://nodejs.org/en/) and [Yarn](https://classic.yarnpkg.com/en/docs/install#windows-stable)
+2. Run `yarn` to install project dependencies
+3. Run `yarn start` to startup the client (please note that much of the functionality will not work without also starting up the backend services)
+4. Navigate to chrome://flags/ and enable the #enable-experimental-web-platform-features flag (This will provide your browser access to serial ports)
 
-If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
+<br />
 
-- Configure the top-level `parserOptions` property like this:
+## Project Structure
 
-```js
-   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-   },
-```
+This react project has the following file structure rules:
+1. Routing is handled from client root in `App.js`, rendered in react by `index.js`
+2. Components which render as a page are generally listed under the `/views` folder
+3. Component styling is placed into a folder under the name of the main component alongside the component and sub-component files. 
+    - As an example the folder `/Home` contains the `Home.js` component, the `Home.css` styling, and a couple of sub-components.
+4. `/Utils` contains additional utility functions like different database requests or authentication handlers
 
-- Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
-- Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
-- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
+<br/>
+
+## Routing
+
+All client routes are handled by react router. The **index.html** file and all static assets will be served by the [server](/server#static-assets).
+
+<br/>
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `yarn start`
+
+Runs the app in the development mode.
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+### `yarn start-build`
+
+Stars a basic http server (using [http-server](https://www.npmjs.com/package/http-server)) which serves the build folder on [http://localhost:3000](http://localhost:3000) .
+
+### `yarn build`
+
+Builds the app for production to the `build` folder.
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+<br />
+
+## Styling
+
+To maintain a consistant theme the folowing has been implemented in `\client\src\assets\style.less` for import
+
+### Colors
+
+    primary: #3D5C82;
+    secondary: #5BABDE;
+    tertiary: #F4F4F5;
+    text-primary: #414141;
+    text-secondary: #FFFFFF;
+
+<br />
+
+### Relevant notes
+
+Section on fixing issue where yarn build does not minify: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+Section on code splitting, worth considering at some point: https://facebook.github.io/create-react-app/docs/code-splitting
--- client-ts/client-retooled/src/App.tsx
+++ client-ts/client-retooled/src/App.tsx
@@ -2,34 +2,125 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import PrivateRoute from './Utils/PrivateRoute';
+import About from './views/About/About';
+import BlocklyPage from './views/BlocklyPage/BlocklyPage';
+import BugReport from './views/BugReport/BugReport';
+import ContentCreator from './views/ContentCreator/ContentCreator';
+import Home from './views/Home/Home';
+import Classroom from './views/Mentor/Classroom/Classroom';
+import Dashboard from './views/Mentor/Dashboard/Dashboard';
+import NotFound from './views/NotFound';
+import Replay from './views/Replay/Replay';
+import ActivityLevelReport from './views/Researcher/ActivityLevelReport';
+import ActivityLevelReportView from './views/Researcher/ActivityLevelReportView';
+import GroupReport from './views/Researcher/GroupReport';
+import Report from './views/Researcher/StudyBrowser';
+import Student from './views/Student/Student';
+import StudentLogin from './views/StudentLogin/StudentLogin';
+import ForgetPassword from './views/TeacherLogin/ForgetPassword';
+import ResetPassword from './views/TeacherLogin/ResetPassword';
+import TeacherLogin from './views/TeacherLogin/TeacherLogin';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/teacherlogin' element={<TeacherLogin/>} />
+        <Route path='/forgot-password' element={<ForgetPassword />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+        <Route path='/login' element={<StudentLogin />} />
+        <Route path='/replay/:saveID' element={<Replay />} />
+        <Route path='/sandbox' element={<BlocklyPage isSandbox={true} />} />
+        <Route
+          path='/report'
+          element={
+            <PrivateRoute>
+              <Report />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path='/activityLevel'
+          element={
+            <PrivateRoute>
+              <ActivityLevelReport />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path='/activityLevel/:id'
+          element={
+            <PrivateRoute>
+              <ActivityLevelReportView />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path='/group-report'
+          element={
+            <PrivateRoute>
+              <GroupReport />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path='/dashboard'
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path='/student'
+          element={
+            <PrivateRoute>
+              <Student />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path='/classroom/:id'
+          element={
+            <PrivateRoute>
+              <Classroom />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path='/workspace'
+          element={
+            <PrivateRoute>
+              <BlocklyPage isSandbox={false} />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path='/activity'
+          element={
+            <PrivateRoute>
+              <BlocklyPage isSandbox={false} />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path='/ccdashboard'
+          element={
+            <PrivateRoute>
+              <ContentCreator />
+            </PrivateRoute>
+          }
+        />
+        <Route path='/bugreport' element={<BugReport />} />
+        <Route path='*' element={<NotFound/>} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
