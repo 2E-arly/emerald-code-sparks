1bbe8b5deaacebb61bf11f95cb22450efbc4fa5d
Michael Tomadakis <c1a55yc0d3r@gmail.com>
2023-11-06 13:03:02 -0500

    migrate utils

--- client-ts/client-retooled/src/App.tsx
+++ client-ts/client-retooled/src/App.tsx
@@ -1,8 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import React from 'react';
+import './App.css'
 import { Route, Routes } from 'react-router-dom';
 import PrivateRoute from './Utils/PrivateRoute';
 import About from './views/About/About';
